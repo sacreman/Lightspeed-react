4856024baa461e170f9bf80e656c90dc3460cd88
sacreman <sacreman@gmail.com>
2021-01-09 16:05:41 +0700

    update chat link

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ const App = () => {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/live_chat?v=lNcfFHCTR-8&embed_domain=maggiemays.live&dark_theme=1"
+                src="https://www.youtube.com/live_chat?v=uX0jCgcXxI4&embed_domain=maggiemays.live&dark_theme=1"
               ></iframe>
             </div>
           </div>
